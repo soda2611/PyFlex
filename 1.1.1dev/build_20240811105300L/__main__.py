0868e2a3582675714e789df75140d7e65f74da92
soda2611 <145172441+soda2611@users.noreply.github.com>
2024-08-11 11:29:08 +0700

    Update build_20240811105300L

---
+++ 1.1.1dev/build_20240811105300L/__main__.py
@@ -0,0 +1,13 @@
+import subprocess
+from build_20240811105300L import __active__
+
+with open("build_20240811105300L.py", encoding="utf-8") as f: startup=f.read()
+
+if input("Enable customized shell (y/n): ").replace(" ", "").lower=="y": customized_shell=True
+else: customized_shell=False
+
+def active(customized_shell=False):
+        if customized_shell: subprocess.Popen(['python', '-i', '-c', f'{startup[:startup.find("if __name__")]}\ncls()'], text=True)
+        else: __active__("cls()")
+
+active(customized_shell)
